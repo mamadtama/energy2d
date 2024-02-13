6ee848dbeea172807dec938d9c8a22023889b636
Mamad Tamamadin <41966900+mamadtama@users.noreply.github.com>
2024-02-14 01:20:40 +0900

    Update energy.js

--- js/energy.js
+++ js/energy.js
@@ -347,32 +347,32 @@
     var tiepoint = image.getTiePoints()[0];
     var pixelScale = image.getFileDirectory().ModelPixelScale;
     var geoTransform = [tiepoint.x, pixelScale[0], 0, tiepoint.y, 0, -1*pixelScale[1]];
-
-
     var pressData = new Array(tiffHeight);
-    var tempData = new Array(tiffHeight);
+    /*var tempData = new Array(tiffHeight);
     var uData = new Array(tiffHeight);
     var vData = new Array(tiffHeight);
-    var spdData = new Array(tiffHeight);
+    var spdData = new Array(tiffHeight);*/
     for (var j = 0; j<tiffHeight; j++){
         pressData[j] = new Array(tiffWidth);
-        tempData[j] = new Array(tiffWidth);
+        /*tempData[j] = new Array(tiffWidth);
         uData[j] = new Array(tiffWidth);
         vData[j] = new Array(tiffWidth);
-        spdData[j] = new Array(tiffWidth);
+        spdData[j] = new Array(tiffWidth);*/
         for (var i = 0; i<tiffWidth; i++){
             pressData[j][i] = rasters[0][i + j*tiffWidth];
-            tempData[j][i] = rasters[1][i + j*tiffWidth];
-            uData[j][i] = rasters[2][i + j*tiffWidth];
+            //tempData[j][i] = rasters[1][i + j*tiffWidth];
+            //uData[j][i] = rasters[2][i + j*tiffWidth];
             //vData[j][i] = rasters[3][i + j*tiffWidth];
-            spdData[j][i] = 1.943844492 * Math.sqrt(uData[j][i]*uData[j][i] + uData[j][i]*uData[j][i]);
-
+            //spdData[j][i] = 1.943844492 * Math.sqrt(uData[j][i]*uData[j][i] + uData[j][i]*uData[j][i]);
         }
     }
 
-    var intervalsSpd = [0, 8, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42,
-      44, 46, 48, 50, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96 ];
-    var bandsWind = rastertools.isobands(spdData, geoTransform, intervalsSpd);
+   // var intervalsSpd = [0, 8, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42,
+   //   44, 46, 48, 50, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96 ];
+    var intervalsPress = [970, 972, 974, 976, 978, 980, 982, 984, 986, 988, 990, 992, 994, 996, 998,
+      1000, 1002, 1004, 1006, 1008, 1010, 1012, 1014, 1016, 1018, 1020, 1022, 1024, 1026, 1028];
+	    
+    var bandsWind = rastertools.isobands(pressData, geoTransform, intervalsPress);
 
     function getColor(d) {
     return d > 92   ? '#643c32' :
@@ -410,17 +410,15 @@
            d > 0   ? '#ffffff' :
                     '#ffffff';
             }
-
-
     function style(feature) {
-    return {
-        fillColor: getColor(feature.properties[0].lowerValue),
-        weight: 2,
-        opacity: 1,
-        color: getColor(feature.properties[0].lowerValue),
-        dashArray: '3',
-        fillOpacity: 0.5
-      };
+	    return {
+		fillColor: getColor(feature.properties[0].lowerValue),
+		weight: 2,
+		opacity: 1,
+		color: getColor(feature.properties[0].lowerValue),
+		dashArray: '3',
+		fillOpacity: 0.5
+	      };
     }
 
     var bandsWindLayer = L.geoJson(bandsWind, {
@@ -434,7 +432,7 @@
 	        sticky: true
 	    });
 	}
-
+/*
     var intervalsPress = [970, 972, 974, 976, 978, 980, 982, 984, 986, 988, 990, 992, 994, 996, 998,
       1000, 1002, 1004, 1006, 1008, 1010, 1012, 1014, 1016, 1018, 1020, 1022, 1024, 1026, 1028];
     var isobars = rastertools.isolines(pressData, geoTransform, intervalsPress);
@@ -445,7 +443,7 @@
           "weight": 2,
           "opacity": 0.65
         }
-    });
+    }); */
 
    //Creating the color scale https://github.com/santilland/plotty/blob/master/src/plotty.js
    var cs_def = {positions:[0.0,0.030303030303,0.0606060606061,0.0909090909091,0.121212121212,0.151515151515,0.181818181818,0.212121212121,0.242424242424,0.272727272727,0.30303030303,0.333333333333,0.363636363636,0.393939393939,0.424242424242,0.454545454545,0.484848484848,0.515151515152,0.545454545455,0.575757575758,0.606060606061,0.636363636364,0.666666666667,0.69696969697,0.727272727273,0.757575757576,0.787878787879,0.818181818182,0.848484848485,0.878787878788,0.909090909091,0.939393939394,0.969696969697,1.0],
@@ -454,6 +452,8 @@
              "#0ea10e", "#1eb31e", "#36d33c", "#50ef50", "#78f572", "#97f58d", "#b5fbab", "#c9ffbf",
              "#ffe978", "#ffc13c", "#ffa100", "#ff6000", "#ff3200", "#e11400", "#c10000", "#a50000",
              "#643c32", "#785046", "#8d645a"]};
+   
+/*	    
    var scaleWidth = 256;
    var canvasColorScale = document.createElement('canvas');
     canvasColorScale.width = scaleWidth;
@@ -531,39 +531,15 @@
 
    var imageLayer = L.imageOverlay(canvasRaster.toDataURL(), imageBounds,{
      opacity: 0.5
-   });
+   }); */
 
-	    /*
-   L.control.layers(null, {
-    "Wind speed": bandsWindLayer,
-    "Pressure": isobarsLayer,
-    "Image": imageLayer
-    }).addTo(map);
-*/
    layerControl.addOverlay(bandsWindLayer, "Energy demand");
-	
-/*
-  map.on('click', function(e) {
-    var xTiff = (e.latlng.lng - geoTransform[0])/geoTransform[1];
-    var yTiff = ( e.latlng.lat - geoTransform[3])/geoTransform[5];
-    var temp = tempData[Math.round(yTiff)][Math.round(xTiff)];
-    var press = pressData[Math.round(yTiff)][Math.round(xTiff)];
-    var uValue = uData[Math.round(yTiff)][Math.round(xTiff)];
-    var vValue = vData[Math.round(yTiff)][Math.round(xTiff)];
-    var spd = Math.sqrt(uValue*uValue + vValue*vValue);
-    var dir = 270 + (Math.atan2(-vValue,uValue)*180/Math.PI);
-    if(dir<0){dir = dir + 360;}
-    if(dir>360){dir = dir - 360;}
 
-    //L.popup().setLatLng(e.latlng).setContent("Wind speed: " + spd ).openOn(map);
-  });*/
   };
   xhr.send();
 
 
-
-
-
+//------------------------------------- Graphic --------------------------------------------------
 (async () => {
 
     const data = await fetch(
