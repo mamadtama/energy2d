3f41a6312f430fbd9a7cec1ae9a3387a3b843cee
Mamad Tamamadin <41966900+mamadtama@users.noreply.github.com>
2024-02-13 23:49:36 +0900

    Update energy.js

--- js/energy.js
+++ js/energy.js
@@ -557,6 +557,73 @@
 
 
 
+
+(async () => {
+
+    const data = await fetch(
+        'data/usdeur.json'
+    ).then(response => response.json());
+
+    Highcharts.chart('containerdemand', {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Energy Demand Forecast',
+            align: 'left'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in',
+            align: 'left'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'USD to EUR',
+            data: data
+        }]
+    });
+})();
+
+
 var retrieved_data = [
   {
     "wkt_geom": "MultiPolygon (((190105 179091, 190105 178891, 189905 178891, 189905 179091, 190105 179091)))",
